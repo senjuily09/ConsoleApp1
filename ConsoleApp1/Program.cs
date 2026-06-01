dc606154161f11e6899316fd126e749dcf7232ef
senjuily09 <gvsharshith14@gmail.com>
2026-06-01 16:55:19 +0530

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void mymethod(string fname)
+        {
+            Console.WriteLine(fname + "world");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string username = Console.ReadLine();
+            Console.WriteLine(username);
+            string[] names = { "Alice", "Bob", "Charlie" };
+            foreach (string i in names)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names.Length);
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine(numbers.Min());
+            Console.WriteLine(numbers.Sum());
+            // Multidimensional array
+            int[,] matrix = { { 1, 2 }, { 3, 4 } };
+            for (int i = 0; i < matrix.Length; i++) {
+                Console.WriteLine(i);
+            }
+            mymethod("hello");
+        }
+    }
+}
